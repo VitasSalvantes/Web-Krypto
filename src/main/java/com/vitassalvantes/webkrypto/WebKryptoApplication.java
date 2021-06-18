1a472f5e1fc04c200a255c75dab944a146f99fd4
VitasSalvantes <salvantesvitas@gmail.com>
2021-06-18 18:38:23 +0200

    Initial commit

---
+++ src/main/java/com/vitassalvantes/webkrypto/WebKryptoApplication.java
@@ -0,0 +1,13 @@
+package com.vitassalvantes.webkrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebKryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebKryptoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vitassalvantes/webkrypto/WebKryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitassalvantes.webkrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebKryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
