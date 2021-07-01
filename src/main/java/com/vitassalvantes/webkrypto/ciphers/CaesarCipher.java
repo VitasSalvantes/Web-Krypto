8c41369aa746492e767615f03e48da41c07b25fd
VitasSalvantes <salvantesvitas@gmail.com>
2021-07-01 11:52:58 +0200

    Optimised code.

--- src/main/java/com/vitassalvantes/webkrypto/ciphers/CaesarCipher.java
+++ src/main/java/com/vitassalvantes/webkrypto/ciphers/CaesarCipher.java
@@ -12,33 +12,17 @@ import java.util.Scanner;
 
 public class CaesarCipher {
 
-    /** Message from user */
-    private String inputMessage;
-
     /** En- or decrypted message */
     private String outputMessage = "";
-
-    /** A Scanner class object that accepts user input */
-    final private Scanner sc = new Scanner(System.in);
 
     /** List of letters of the English alphabet */
     final private ArrayList<Character> englishAlphabet = new ArrayList<Character>();
 
-    /** A character array containing the processed custom message */
-    private char[] chars;
+    /** Message from user */
+    private char[] inputMessage;
 
     /** Key used for encryption */
     private int key;
-
-    /** Method that launches the program */
-    public static void main(String[] args) {
-        CaesarCipher cc = new CaesarCipher();
-
-        cc.setInputMessage();
-        cc.setKey();
-        System.out.println(cc.decryption(cc.getInputMessage(), cc.getKey()));
-        System.out.println(cc.encryption(cc.getInputMessage(), cc.getKey()));
-    }
 
     /** Method for creating letters of the English alphabet */
     private void createEnglishAlphabet() {
@@ -47,37 +31,14 @@ public class CaesarCipher {
         }
     }
 
-    /**
-     * Method handling custom message
-     * 
-     * @return chars {@link CaesarCipher#chars}
-     */
-    private char[] getInputMessage() {
-        chars = inputMessage.toCharArray();
-        return chars;
-    }
-
-    /**
-     * Method that receives and processes a custom key
-     * 
-     * @return key {@link CaesarCipher#key}
-     */
-    private int getKey() {
-        key %= 25;
-
-        return key;
-    }
-
     /** Setter for inputMessage {@link CaesarCipher#inputMessage} */
-    private void setInputMessage() {
-        System.out.println("Your message:");
-        inputMessage = sc.nextLine().toLowerCase();
+    public void setInputMessage(String inputMessage) {
+        this.inputMessage = inputMessage.toLowerCase().toCharArray();
     }
 
     /** Setter for key {@link CaesarCipher#key} */
-    private void setKey() {
-        System.out.println("Your key:");
-        key = Math.abs(sc.nextInt());
+    public void setKey(int key) {
+        this.key = Math.abs(key % 25);
     }
 
     /**
@@ -85,13 +46,11 @@ public class CaesarCipher {
      * 
      * @return outputMessage {@link CaesarCipher#outputMessage}
      */
-    private String encryption(char[] chars, int key) {
-        chars = this.chars;
+    public String encryption() {
         createEnglishAlphabet();
-        key = this.key;
 
-        for (char c : chars) {
-            if (englishAlphabet.contains(c) == true) {
+        for (char c : inputMessage) {
+            if (englishAlphabet.contains(c)) {
                 if ((englishAlphabet.indexOf(c) + key) > 25) {
                     outputMessage += englishAlphabet.get(Math.abs(26 - (englishAlphabet.indexOf(c) + key)));
                     continue;
@@ -103,7 +62,6 @@ public class CaesarCipher {
             }
         }
 
-        sc.close();
         return outputMessage;
     }
 
@@ -112,13 +70,11 @@ public class CaesarCipher {
      * 
      * @return outputMessage {@link CaesarCipher#outputMessage}
      */
-    private String decryption(char[] chars, int key) {
-        chars = this.chars;
+    public String decryption() {
         createEnglishAlphabet();
-        key = this.key;
 
-        for (char c : chars) {
-            if (englishAlphabet.contains(c) == true) {
+        for (char c : inputMessage) {
+            if (englishAlphabet.contains(c)) {
                 if ((englishAlphabet.indexOf(c) - key) < 0) {
                     outputMessage += englishAlphabet.get(Math.abs(26 - (key - englishAlphabet.indexOf(c))));
                     continue;
@@ -130,7 +86,6 @@ public class CaesarCipher {
             }
         }
 
-        sc.close();
         return outputMessage;
     }
 }
